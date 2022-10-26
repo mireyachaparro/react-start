a3dfdfd3661a14512b2f80274ced155b1603b90d
mireyachaparro <mireyacn99@gmail.com>
2022-10-26 12:13:54 +0200

    Update initial config

---
+++ src/infrastructure/components/app/app.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "../layout/layout";
+import "./app.css";
+
+export function App() {
+  return (
+    <Layout>
+      <div className="app">Probando</div>
+    </Layout>
+  );
+}
